9f4bc6f7cabce2acca2dbd98c864d6eac214a99b
Frank Raso <frankraso91@gmail.com>
2022-06-11 16:31:58 -0400

    add css to App,import body

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
+import Body from '../Body/Body';
+
 
 function App() {
-    return (
+  return (
+    <div>
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
+          <h1 className="App-title">Gallery of Franks Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
       </div>
-    );
+      <Body />
+    </div>
+  );
 }
 
 export default App;
